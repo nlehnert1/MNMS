c897cb77fa741acc22e75bbd97b2839821cf2e16
Nick Lehnert <nick.lehnert1@gmail.com>
2019-02-09 00:02:03 -0600

    Moved script to folder, created skeleton of health controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    float horizAxis;
+    float vertAxis;
+
+    float moveDist;
+
+    bool sprint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveDist = 5.0f;
+        sprint = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sprint = Input.GetButton("XboxX");
+        Translate();
+        vertAxis = Input.GetAxisRaw("Vertical");
+        horizAxis = Input.GetAxisRaw("Horizontal");
+ 
+        
+        
+    }
+
+    void Translate()
+    {
+        //TODO
+        if (sprint)
+        {
+            moveDist = 20.0f;
+        }
+        else
+        {
+            moveDist = 5.0f;
+        }
+        //Go Up+Left
+        if (vertAxis > 0 && horizAxis < 0)
+        {
+            this.transform.Translate(((Vector2.up * vertAxis) + (Vector2.left * -horizAxis)) * moveDist * Time.deltaTime);
+        }
+
+        //Go Up+Right
+        else if(vertAxis > 0 && horizAxis > 0)
+        {
+            this.transform.Translate(((Vector2.up * vertAxis) + (Vector2.right * horizAxis)) * moveDist * Time.deltaTime); 
+        }
+
+        //Go Down+Left
+        else if(vertAxis < 0 && horizAxis < 0)
+        {
+            this.transform.Translate(((Vector2.down * -vertAxis) + (Vector2.left * -horizAxis)) * moveDist * Time.deltaTime);
+        }
+
+        //Go Down+Right
+        else if (vertAxis < 0 && horizAxis > 0)
+        {
+            this.transform.Translate(((Vector2.down * -vertAxis) + (Vector2.right * horizAxis)) * moveDist * Time.deltaTime);
+        }
+        //Go Up
+        else if(vertAxis > 0)
+        {
+            this.transform.Translate(((Vector2.up * vertAxis) * vertAxis) * moveDist * Time.deltaTime);
+        }
+
+        //Go Down
+        else if(vertAxis < 0)
+        {
+            this.transform.Translate((Vector2.down * -vertAxis) * moveDist * Time.deltaTime);
+        }
+
+        //Go Left
+        else if(horizAxis < 0)
+        {
+            this.transform.Translate((Vector2.left * -horizAxis) * moveDist * Time.deltaTime);
+        }
+
+        //Go Right
+        else if(horizAxis > 0)
+        {
+            this.transform.Translate((Vector2.right * horizAxis) * moveDist * Time.deltaTime);
+        }
+    }
+}
