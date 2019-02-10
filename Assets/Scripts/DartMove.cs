ea119d76fd00ff72cc65569d5e15e4327e56947a
Nick Lehnert <nick.lehnert1@gmail.com>
2019-02-09 23:01:01 -0600

    Implemented Darts

---
+++ Assets/Scripts/DartMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DartMove : MonoBehaviour
+{
+	public float speed = .05f;
+	public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+		rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		rb.velocity = gameObject.transform.up * speed;
+    }
+	/*
+	private void OnCollisionEnter2D(Collider2D other){
+		print("DART IS GONE");
+		//if(other.gameObject.tag == "Dart"){
+			//other.gameObject.Destroy;
+			Destroy (gameObject);
+		}
+	}
+	*/
+}
+
---
+++ Assets/Scripts/DartShooter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DartShooter : MonoBehaviour
+{
+	public double timeBetweenShots;
+	public GameObject darts;
+	double timeStamp = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(timeStamp < Time.time){
+			//fires the darts
+			timeStamp = Time.time + timeBetweenShots;
+			Instantiate(darts, transform.position,transform.rotation, transform);
+		}
+    }
+}
