d31e0a6830c4653b1b81891e7b29a312944c352e
SirMrDrProfSrJrPhD <30580617+SirMrDrProfSrJrPhD@users.noreply.github.com>
2019-02-09 20:07:42 -0600

    Added CheckPoint System Fully
    Here it is!

---
+++ Assets/Scripts/HalfwayPoint.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HalfwayPoint : MonoBehaviour
+{
+	public bool isTouched = false; 
+	public GameObject[] checkPoints;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnTriggerEnter2D(Collider2D other){
+		//Each checkpoint can be gained once until the end is reached, upon which they are all reset
+		if(other.gameObject.tag == "Player"){
+			if(isTouched == false){
+				checkPoints = GameObject.FindGameObjectsWithTag("CheckPoint");
+				isTouched = true;
+
+			}
+
+
+			foreach(GameObject checkP in checkPoints){
+				checkP.GetComponent<checkPointTrack>().goingBack = true;
+				checkP.GetComponent<checkPointTrack>().hasTouched = false;
+			}
+		}
+	}
+}
--- Assets/Scripts/checkPointTrack.cs
+++ Assets/Scripts/checkPointTrack.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class checkPointTrack : MonoBehaviour
 {
     // Start is called before the first frame update
+	public bool hasTouched = false;
+	public bool goingBack = false;
+	public GameObject mCamera; 
     void Start()
     {
-        
+		mCamera = GameObject.Find("Main Camera");
     }
 
     // Update is called once per frame
@@ -17,10 +20,31 @@ public class checkPointTrack : MonoBehaviour
     }
 
 	private void OnTriggerEnter2D(Collider2D other){
-		print("enter");
+		//Each checkpoint can be gained once until the end is reached, upon which they are all reset
 		if(other.gameObject.tag == "Player"){
-			other.gameObject.GetComponent<checkPointManager>().currentCPoint = gameObject;
+			if(hasTouched == false){
+				other.gameObject.GetComponent<checkPointManager>().currentCPoint = gameObject;
+				other.gameObject.GetComponent<checkPointManager>().checkPointCounter++;
+				hasTouched = true;
+				if(goingBack == false){
+					//move camera forward
+					mCamera.transform.position += new Vector3(0f,10f,0f);
+				} else {
+					mCamera.transform.position -= new Vector3(0f,10f,0f);
+				}
+			}
 
+		}
+	}
+	private void OnTriggerStay2D(Collider2D other){
+		
+		if(other.gameObject.tag == "Player"){
+			if(goingBack == false){
+				//move camera forward
+				other.gameObject.transform.position += new Vector3(0f,.1f,0f);
+			} else {
+				other.gameObject.transform.position -= new Vector3(0f,.1f,0f);
+			}
 		}
 	}
 }
